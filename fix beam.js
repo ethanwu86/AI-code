f069b8d1ae4830d501fdcfc37c1c1629fb6c5657
Ethan <wuyusheng86@hotmail.com>
2018-03-08 23:05:05 +0400

    create js file

---
+++ fix beam.js
@@ -0,0 +1,13 @@
+
+
+var nbelems = ixforten_model.nb_elems();
+for (var i = 0; i < nbelems; i++) {
+	var obj = ixforten_model.element(i);
+	if (obj.get_property("fem_type") == "beam") {
+		var nodeA = obj.node(0);
+		var nodeB = obj.node(1);
+		nodeA.set_property("restraint", 63);
+		nodeB.set_property("restraint", 63);
+		
+	}
+}
